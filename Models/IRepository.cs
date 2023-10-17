f703b8f0ad385843d1da03a426a1cc05fd55ed14
Oktysutc <126988680+Oktysutc@users.noreply.github.com>
2023-10-17 14:43:19 +0300

    .

--- Models/IRepository.cs
+++ Models/IRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace MvcProje.Models
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace MvcProje.Models
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
+        //T->kitapturu
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filtre);
+        void Ekle(T entity);
+        void Sil(T entity);
+        void SilAralık(IEnumerable<T> entities);
     }
 }
