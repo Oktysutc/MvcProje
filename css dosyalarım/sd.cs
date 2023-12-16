ca3406462837b48626f61a270621451dde0d7e94
oktay sütçü <126988680+Oktysutc@users.noreply.github.com>
2023-12-16 17:11:55 +0300

    Add files via upload

---
+++ css dosyalarım/sd.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace form._12._4
+{
+    class oktay{
+        Console.WriteLine("kmxkwmxkw");
+    }
+}
